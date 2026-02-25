7d3f80359ba36f734a31671acb8ff806151b6102
mollynorman <norman.j.molly@gmail.com>
2026-02-25 10:56:40 -0600

    chore: create iOS Project

---
+++ iOS/TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Molly Norman on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
