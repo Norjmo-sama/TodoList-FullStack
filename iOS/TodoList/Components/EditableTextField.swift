64ff582016288626cf741c34fc4e9c0066febcf0
mollynorman <norman.j.molly@gmail.com>
2026-03-11 11:24:43 -0500

    feat: Implemented EditableTextField, ItemEditorOverlay, HomePage, LoadingPage, and LoginPage components

---
+++ iOS/TodoList/Components/EditableTextField.swift
@@ -0,0 +1,40 @@
+//
+//  EditableTextField.swift
+//  TodoList
+//
+//  Created by Molly Norman on 2/27/26.
+//
+
+import SwiftUI
+
+struct EditableTextField: View {
+    @Binding var text: String
+    @State var isEditing: Bool = false
+    var body: some View {
+        HStack {
+            if(isEditing) {
+               TextField("", text: $text)
+                Spacer()
+                Image(systemName: "checkmark")
+                    .onTapGesture {
+                        isEditing = false
+                    }
+            } else {
+                Text(text)
+                Spacer()
+                Image(systemName: "pencil.circle")
+                    .onTapGesture {
+                        isEditing = true
+                    }
+            }
+        }.padding()
+    }
+}
+
+#Preview {
+    @Previewable @State var text: String = ""
+    VStack {
+        EditableTextField(text: $text)
+        EditableTextField(text: $text, isEditing: true)
+    }
+}
